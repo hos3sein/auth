3ce1040a2b942004e6e4bbde1c5364cdf725ee76
hossein <kianlucifer0098@gmail.com>
2024-08-03 18:18:39 +0330

    ref

--- utils/sendSms.js
+++ utils/sendSms.js
@@ -1,11 +1,13 @@
 
+const accountSid = 'AC44d884b4bc1dc051e7e6af0625174160' 
+const authToken = '1254f6ceb148d038fe8dd06d5c304ec2';
 
-  const client = require('twilio')(accountSid, authToken); 
+const client = require('twilio')(accountSid, authToken); 
 
 const sendSms = async (phone,code) => {
    console.log("rrestrrrsrsr");
     console.log("rrestrrrsrsr",phone,code);
- const result= await client.messages.create({ body:code, to:phone})
+ const result= await client.messages.create({ body:code, messagingServiceSid:'MG4180185e3892397701d7de635039f782' ,to:phone})
  console.log("SMS Sent with Resault>>>>>>>>>>>>",result);
  return result
 }
